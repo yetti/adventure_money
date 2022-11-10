5f9e555e866c4c7fef5186ef0efe9295768fb019
Yetrina Battad <hello@yetti.io>
2022-11-10 21:56:35 +1100

    feat: integrate rodauth authentication

--- Gemfile
+++ Gemfile
@@ -77,6 +77,8 @@ gem "view_component", "~> 2.74"
 gem "view_component-contrib", "~> 0.1.1"
 gem "lookbook", "~> 1.1"
 
+gem "rodauth-rails", "~> 1.6"
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[mri mingw x64_mingw]
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,15 @@ Rails.application.routes.draw do
     mount Lookbook::Engine, at: "/lookbook"
   end
 
-  get "owed", controller: "page", action: "owed"
-  get "summary", controller: "page", action: "summary"
-  get "payments", controller: "page", action: "payments"
-  get "categories", controller: "page", action: "categories"
-  get "people", controller: "page", action: "people"
+  get "page/home"
+
+  constraints Rodauth::Rails.authenticated do
+    get "owed", controller: "page", action: "owed"
+    get "summary", controller: "page", action: "summary"
+    get "payments", controller: "page", action: "payments"
+    get "categories", controller: "page", action: "categories"
+    get "people", controller: "page", action: "people"
+  end
 
   root "page#home"
 end
