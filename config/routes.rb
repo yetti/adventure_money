4b5d34caf47e77d26cedfffe199192129f34e009
Yetrina Battad <hello@yetti.io>
2022-11-19 13:25:48 +1100

    feat: adds TailwindCSS

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 Rails.application.routes.draw do
+  get "pages/home"
+  get "pages/owed"
+  get "pages/summary"
+  get "pages/payments"
+  get "pages/categories"
+  get "pages/people"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "pages#home"
 end
