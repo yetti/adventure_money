0900cf8ffd90997dce53b1a9b987bd9279db514f
Yetrina Battad <hello@yetti.io>
2023-01-21 13:36:30 +1100

    feat: adds homepage index controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_scope :user do
-    # redirect users back sign-in
-    get "users", to: "devise/sessions#new"
-  end
-  devise_for :users
-
+  get "home/index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   get "/404", to: "errors#not_found"
   get "/422", to: "errors#unprocessable"
   get "/500", to: "errors#internal_server"
 
   # Defines the root path route ("/")
-  root "pages#home"
+  root "home#index"
 end
