cb2162520dfae474ed2348508cd28655703129b8
Yetrina Battad <hello@yetti.io>
2023-02-09 14:41:17 +1100

    feat: set mailer default URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,8 @@ Rails.application.configure do
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "adventure_money_production"
 
+  config.action_mailer.default_url_options = {host: "https://adventure-money.onrender.com/"}
+
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
