41a64aa8cef12d25c460f13b0561eb6930f2f059
Yetrina Battad <hello@yetti.io>
2023-05-21 12:07:30 +1000

    feat: integrate TailwindCSS as a PostCSS plugin

--- db/schema.rb
+++ db/schema.rb
@@ -13,4 +13,5 @@
 ActiveRecord::Schema[7.1].define(version: 0) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
 end
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: [
+    './app/views/**/*.html.erb',
+    './app/helpers/**/*.rb',
+    './app/assets/stylesheets/**/*.css',
+    './app/javascript/**/*.js'
+  ],
+  plugins: [
+    require('@tailwindcss/typography'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/container-queries'),
+    require('@tailwindcss/line-clamp'),
+  ]
+}
