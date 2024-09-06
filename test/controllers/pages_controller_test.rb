c98364771e3b9c7032def4d373453563e990a35b
Yetrina Battad <hello@yetti.io>
2024-09-06 23:29:18 +0000

    feat: generate static page controller and home page

---
+++ test/controllers/pages_controller_test.rb
@@ -0,0 +1,13 @@
+require "test_helper"
+
+class PagesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get pages_index_url
+    assert_response :success
+  end
+
+  test "root path should get index" do
+    get root_url
+    assert_response :success
+  end
+end
