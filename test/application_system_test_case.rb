ec30117453b89b02b33a820012057482733e33f9
Yetrina Battad <hello@yetti.io>
2024-11-15 19:22:29 +0000

    chore: chore: initial commit

---
+++ test/application_system_test_case.rb
@@ -0,0 +1,23 @@
+require "test_helper"
+require "support/with_clues"
+
+Dir[File.join(__dir__, "system/support/**/*.rb")].sort.each { |file| require file }
+
+class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
+  include TestSupport::WithClues
+
+  driven_by :rack_test
+end
+
+class BrowserSystemTestCase < ApplicationSystemTestCase
+  if ENV["CAPYBARA_SERVER_PORT"]
+    served_by host: "rails-app", port: ENV["CAPYBARA_SERVER_PORT"]
+
+    driven_by :better_cuprite, using: :headless_chrome, screen_size: [ 1400, 1400 ], options: {
+      browser: :remote,
+      url: "http://#{ENV["SELENIUM_HOST"]}:4444"
+    }
+  else
+    driven_by :better_cuprite
+  end
+end
