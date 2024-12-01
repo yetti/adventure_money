a0d3db4d42e36338f8d163a9e1dda9f30159ff02
Yetrina Battad <hello@yetti.io>
2024-12-01 05:47:27 +0000

    feat: style flashes with Bootstrap alerts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,15 @@
 # frozen_string_literal: true
 
 module ApplicationHelper
+  def flash_class(key)
+    case key
+    when "notice" then "alert-success"
+    when "alert" then "alert-danger"
+    when "info" then "alert-info"
+    when "warning" then "alert-warning"
+    when "error" then "alert-danger"
+    when "success" then "alert-success"
+    else "alert-info"
+    end
+  end
 end
